5a9ee4ae19adca3e07b3f4d4bba5fc15b5ffe66d
Sepu <seba.sepu05@gmail.com>
2022-09-27 22:33:07 -0300

    Actualización de activities, ajuste de margenes, orden de elementos

--- app/src/main/java/com/example/rukaffe/IngresoPedido.java
+++ app/src/main/java/com/example/rukaffe/IngresoPedido.java
@@ -2,7 +2,9 @@ package com.example.rukaffe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IngresoPedido extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class IngresoPedido extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_pedido);
+    }
+    public void onClickRegresoaMain(View view) {
+        Intent i = new Intent(IngresoPedido.this, MainMenu.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/rukaffe/IngresoProductos.java
+++ app/src/main/java/com/example/rukaffe/IngresoProductos.java
@@ -2,7 +2,9 @@ package com.example.rukaffe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IngresoProductos extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class IngresoProductos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_productos);
+    }
+    public void onClickRegresoaMain(View view) {
+        Intent i = new Intent(IngresoProductos.this, Inventario.class);
+        startActivity(i);
     }
 }
